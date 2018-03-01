ae4250ecc2d74ba3ef82ed5b923e6bee7b699fc1
Benjamin-G <brgeyer49@gmail.com>
2018-03-01 08:28:23 -0800

    Started add user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,5 +4,19 @@ module Api::V1
       @users = User.all
       render json: @users
     end
+
+    def create
+      @user = User.create(user_params)
+      render json: @user
+    end
+
+
+
+    private
+
+      def user_params
+        params.require(:idea). permit(:user_id, :firstName, :lastName, :title, :manager_id)
+      end
+
   end
 end
